ed35991da3eb82f2e5ead85b3051152bb4006459
Daniel Maina <dmaina@AD.APHRC.ORG>
2025-10-20 12:34:52 +0300

    Addition of R-scripts

--- R-Scripts/Poisson.R
+++ R-Scripts/Poisson.R
@@ -6,3 +6,6 @@ df_processed <- read.csv("D:/APHRC/GoogleDrive_ii/stata_do_files/madiva/stroke_w
 # View data
 View(df_processed)
 
+
+glm(df_processed$stroke_status_derived ~ df_processed$hpt_status_derived, data=df_processed, family = "poisson")
+
