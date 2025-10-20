122da41cf41b480f19f385b86c8aa4032e32729c
Daniel Maina <dmaina@AD.APHRC.ORG>
2025-10-20 12:29:58 +0300

    Addition of R-scripts

---
+++ R-Scripts/Poisson.R
@@ -0,0 +1,8 @@
+# Author:   Daniel Maina Nderitu
+# Year:     2025-Oct
+# Project:  Stroke Analysis
+# Import data
+df_processed <- read.csv("D:/APHRC/GoogleDrive_ii/stata_do_files/madiva/stroke_work/data/df_processed.csv")
+# View data
+View(df_processed)
+
